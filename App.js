b01816aec109615461dfe69cc3c359e1f458502f
Cinara Neis <80927757+cinara-neis@users.noreply.github.com>
2022-01-17 15:33:15 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import Conversor from './src/Conversor';
+
+class App extends Component{
+  render(){
+    return(
+      <View style={styles.container}> 
+        <Conversor moedaA="USD" moedaB="BRL" />
+        <Conversor moedaA="EUR" moedaB="BRL" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  }
+});
+
+export default App;
